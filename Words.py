a89e211224f51e47ea152a0cb2033d18af6ff0ba
Josh-Way <ZJWF103@live.rhul.ac.uk>
2021-08-15 17:47:28 +0100

    added more words to word list

--- Words.py
+++ Words.py
@@ -1,1 +1,1 @@
-Words = ["black", "apple", "ran"]
+Words = ["black", "apple", "ran", "moose", "violin", "cow", "yellow", "seven", "orange", "wolf", "frog", "germany", "helicopter", "cloud", "winter"] 
